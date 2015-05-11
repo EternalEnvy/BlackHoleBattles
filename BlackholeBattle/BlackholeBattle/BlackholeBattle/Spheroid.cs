b61584516f0e534e352749b92c0e7fbfbcee2546
EternalEnvy <lee.southerst@gmail.com>
2015-05-11 18:11:03 -0400

    Edited coolision code
    Accounting for angle of coolision

--- BlackholeBattle/BlackholeBattle/BlackholeBattle/Spheroid.cs
+++ BlackholeBattle/BlackholeBattle/BlackholeBattle/Spheroid.cs
@@ -13,11 +13,26 @@ namespace BlackholeBattle
         Vector3 position;
         Vector3 velocity;
         Vector3 acceleration;
-        public Vector3 Collide(double objectMass, Vector3 vec)
+        public Vector3 Collide(double objectMass, Vector3 vec, Vector3 pos)
         {
             //http://farside.ph.utexas.edu/teaching/301/lectures/node76.html
             //given initla velocities and masses, find final velocity. Modified to find velocity in x,y,and z direction
-            return new Vector3((float)((mass - objectMass) * velocity.X / (mass + objectMass) + 2 * objectMass * vec.X / (mass + objectMass)), (float)((mass - objectMass) * velocity.Y / (mass + objectMass) + 2 * objectMass * vec.Y / (mass + objectMass)), (float)((mass - objectMass) * velocity.Z / (mass + objectMass) + 2 * objectMass * vec.Z / (mass + objectMass)));
+            float velocityX = (float)((mass - objectMass) * velocity.X / (mass + objectMass) + 2 * objectMass * vec.X / (mass + objectMass));
+            float velocityY = (float)((mass - objectMass) * velocity.Y / (mass + objectMass) + 2 * objectMass * vec.Y / (mass + objectMass));
+            float velocityZ = (float)((mass - objectMass) * velocity.Z / (mass + objectMass) + 2 * objectMass * vec.Z / (mass + objectMass));
+
+            velocityY += 2 * (float)Math.Sin(pos.Y - position.Y);
+            velocityZ += 2 * (float)Math.Sin(pos.Z - position.Z);
+            velocityX += 2 * (float)Math.Sin(pos.X - position.X);
+
+            velocityX -= (float)Math.Cos(pos.Y - position.Y);
+            velocityX -= (float)Math.Cos(pos.Z - position.Z);
+
+            velocityY -= (float)Math.Cos(pos.X - position.X);
+            velocityY -= (float)Math.Cos(pos.Z - position.Z);
+
+            velocityZ -= (float)Math.Cos(pos.X - position.X);
+            velocityZ -= (float)Math.Cos(pos.Y - position.Y);
         }
     }
 }
